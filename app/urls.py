91f79e164e85c3bd842f88b2538aa2efd34c16d4
Osvaldo <osvaldosdj@gmail.com>
2022-07-30 16:52:55 -0300

    Cadastro Login e autenticação nas paginas

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from app.views import item_list, item_create, tipo_create
+from app.views import item_list, item_create, tipo_create, login, cadUsuario
 
 urlpatterns =[
     path('item_list/', item_list),
     path('item_create/', item_create),
-    path('tipo_create/', tipo_create)
+    path('tipo_create/', tipo_create),
+    path('login/', login, name="login"),
+    path('cadUsuario/', cadUsuario, name='cadUsuario')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
+from ast import Return
+from django.http.response import HttpResponse
 from django.forms import ModelForm
 from django.shortcuts import render, redirect
 from app.models import Item, Tipo
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def item_list(request):
@@ -30,7 +36,7 @@ class ItemForm(ModelForm):
         widget=forms.CheckboxSelectMultiple
     )
 
-
+@login_required
 def item_create(request):
     form = ItemForm(request.POST or None)
     if form.is_valid():
@@ -52,7 +58,7 @@ class TipoForm(ModelForm):
         """ fields = '__all__' """
     
    
-
+@login_required (login_url="/login")
 def tipo_create(request):
     form = TipoForm(request.POST or None)
     if form.is_valid():
@@ -65,3 +71,36 @@ def tipo_create(request):
              'form': form
         }
     )
+
+def login(request):
+    if request.method =="GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get ('username')
+        senha = request.POST.get ('senha')
+
+    user = authenticate(username=username, password=senha)
+    
+    if user:
+        return HttpResponse('Usuário Autenticado')
+    else:
+        return HttpResponse('Usuario ou Senha incorretos')
+
+
+@login_required(login_url="/login")
+def cadUsuario(request):
+    if request.method == "GET":
+        return render (request, 'cadUsuario.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        passwd = request.POST.get('senha')
+    
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Usuario Já cadastrado')
+        user = User.objects.create_user(username=username, email=email, password=passwd)
+        user.save()
+
+        return HttpResponse ('Usuario cadastrado com sucesso')
